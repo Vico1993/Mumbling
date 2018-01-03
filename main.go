d12821de7f57c2429190bb9bbd6e48e16c1d2ae7
Victor Piolin <victor.piolin@gmail.com>
2018-01-03 20:54:36 +0100

    Create function accum

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+func accum(s string) {
+	result := make([]string, len(s))
+	arrString := strings.SplitAfter(s, "")
+	for i, l := range arrString {
+		var buffer bytes.Buffer
+		for j := 1; j <= i+1; j++ {
+			buffer.WriteString(l)
+		}
+		result[i] = strings.Title(buffer.String())
+	}
+	fmt.Println(strings.Join(result, "-"))
+}
+
+func main() {
+	accum("abcd")
+	accum("RqaEzty")
+	accum("cwAt")
+}
